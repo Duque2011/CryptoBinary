3e2f791399ec97d73e440749b94d3fb9bcda781a
wilmirram <wmrmidias@gmail.com>
2025-03-08 16:40:02 -0300

    testing

--- src/components/pages/payment/SelectWalletType.tsx
+++ src/components/pages/payment/SelectWalletType.tsx
@@ -22,6 +22,9 @@ export const SelectWalletType = ({
         <p className="text-sm text-muted-400">
           {t("Pick one of the following wallet types to continue")}
         </p>
+        <p className="text-sm text-muted-400">
+          {t("RAMIRES")}
+        </p>
       </div>
 
       <div className="mx-auto mb-4 w-full max-w-lg rounded-sm px-8 pb-8">
