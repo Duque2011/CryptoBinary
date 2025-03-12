fabdda075927d24c8444100f58a8e8d1d43c81af
wilmirram <wmrmidias@gmail.com>
2025-03-11 22:30:18 -0300

    update

--- backend/api/admin/finance/investment/plan/structure.get.ts
+++ backend/api/admin/finance/investment/plan/structure.get.ts
@@ -161,7 +161,7 @@ export const investmentPlanStructure = async () => {
     name: "walletType",
     options: [
       { value: "FIAT", label: "Fiat" },
-      { value: "SPOT", label: "Binária" },
+      { value: "Binária", label: "Binária" },
     ],
     placeholder: "Select wallet type",
   };
